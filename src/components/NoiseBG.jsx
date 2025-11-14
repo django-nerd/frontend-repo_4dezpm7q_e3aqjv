766220bb90eea084f718e20ec11b377304bbe544
Flames <vibe-coder@flames.com>
2025-11-14 03:56:23 +0000

    Update project prj_4dezpm7q - 2025-11-14T03:56:22.519Z

--- src/components/NoiseBG.jsx
+++ src/components/NoiseBG.jsx
@@ -1,9 +1,16 @@
 export default function NoiseBG() {
+  const svg = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="160" height="160" viewBox="0 0 160 160">
+      <filter id="n"><feTurbulence type="fractalNoise" baseFrequency=".8" numOctaves="2"/></filter>
+      <rect width="100%" height="100%" filter="url(#n)" opacity=".5"/>
+    </svg>
+  `;
+  const dataUrl = `url("data:image/svg+xml,${encodeURIComponent(svg)}")`;
+
   return (
-    <div className="pointer-events-none fixed inset-0 -z-10 opacity-[0.06] mix-blend-soft-light dark:opacity-[0.08]" style={{ backgroundImage: 'url("data:image/svg+xml;utf8,\
-      <svg xmlns=\'http://www.w3.org/2000/svg\' width=\'160\' height=\'160\' viewBox=\'0 0 160 160\'>\
-        <filter id=\'n\'><feTurbulence type=\'fractalNoise\' baseFrequency=\'.8\' numOctaves=\'2\'/></filter>\
-        <rect width=\'100%\' height=\'100%\' filter=\'url(%23n)\' opacity=\'.5'/>\
-      </svg>\n")', backgroundSize: '160px 160px' }} />
-  )
+    <div
+      className="pointer-events-none fixed inset-0 -z-10 opacity-[0.06] mix-blend-soft-light dark:opacity-[0.08]"
+      style={{ backgroundImage: dataUrl, backgroundSize: '160px 160px' }}
+    />
+  );
 }
